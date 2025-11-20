18e35bdae2c5869b2261fc33c2e381e781f3aa01
devaidusty <devainerd@gmail.com>
2025-11-20 09:00:02 +0000

    Update Firebase configuration with new keys

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// Replace the old line with this (using YOUR keys):
+const firebaseConfig = {
+  apiKey: "AIzaSyAwY5tI1t5N0iFAihcsRElv75M1Y3lCaZU",
+  authDomain: "leaveos.firebaseapp.com",
+  projectId: "leaveos",
+  storageBucket: "leaveos.firebasestorage.app",
+  messagingSenderId: "423677575028",
+  appId: "1:423677575028:web:e7d71f2745ab90c04e3621",
+  measurementId: "G-2NCCP6JVG8"
+};
