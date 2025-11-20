a190d5b66d21e606d05e4d82539141604baeea36
devaidusty <devainerd@gmail.com>
2025-11-20 11:17:31 +0000

    Update Firebase configuration in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import React from 'react';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAwY5tI1t5N0iFAihcsRElv75M1Y3lCaZU",
+  apiKey: "AIzaSyAwY5t1t5N0fAihcsRELV75MY13LcaZU",
   authDomain: "leaveos.firebaseapp.com",
   projectId: "leaveos",
   storageBucket: "leaveos.firebasestorage.app",
   messagingSenderId: "423677575028",
-  appId: "1:423677575028:web:e7d71f2745ab90c04e3621",
-  measurementId: "G-2NCCP6JVG8"
+  appId: "1:423677575028:web:e7d17f2f45ab9cc04e3621",
+  measurementId: "G-2NCPGJVG8"
 };
+
+function App() {
+  return (
+    <div>
+      <h1>LeaveOS Platform</h1>
+    </div>
+  );
+}
+
+export default App;
